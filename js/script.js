f516fc3d4b3af70442349782d9cbdf4ca70aae11
Mile Gombo <yamilegombo30@gmail.com>
2024-08-29 20:00:28 +0900

    komit kedua

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// single line comment
+
+/*
+ multiple line comment
+ bisa tulis juga disini
+*/
+
+
+var namaDepang ="Yamile";
+var namaBelakang ="Gombo";
+
+document.getElementById(nama)
+innerHTML =namaDepang + namaBelakang;
+
+
